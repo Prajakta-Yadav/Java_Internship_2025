5f9f48748c7436c064edeadbaec13fb44c15cb60
Prajakta  Yadav <88527873+Prajakta-Yadav@users.noreply.github.com>
2025-07-28 17:04:00 +0530

    Create  EvenOddNum.java

---
+++ Conditional_Control/EvenOddNum.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class EvenOddNum {
+
+    // If else conditional Statement 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num = sc.nextInt();
+        sc.close();
+        if(num == 0){
+             System.out.println("The number " + num + " is a neutral.");
+        }
+        else if(num % 2 ==0){
+            System.out.println("The number " + num + " is Even.");
+        }
+        else {
+            System.out.println("The number " + num + " is Odd.");
+        }
+    }
+}
