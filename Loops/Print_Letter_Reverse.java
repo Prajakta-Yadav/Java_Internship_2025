55c3f1970a457ab274bf17d82f7450f941ac2171
Prajakta  Yadav <88527873+Prajakta-Yadav@users.noreply.github.com>
2025-08-14 03:30:14 -0700

    Create Print_Letter_Reverse.java

---
+++ Loops/Print_Letter_Reverse.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Print_Letter_Reverse{
+    public static void main(String[] args){
+    
+    char ch = 'Z';
+    
+    System.out.println("Printing Letters Z to A");
+    
+    while(ch >= 'A'){
+        System.out.println(ch);
+        ch--;
+    }
+    
+}
+}
