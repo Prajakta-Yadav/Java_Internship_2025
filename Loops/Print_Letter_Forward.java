93498ea149061db61f99409faa8f191076e10590
Prajakta  Yadav <88527873+Prajakta-Yadav@users.noreply.github.com>
2025-08-14 03:19:21 -0700

    Create Print_Letter_Forward.java

---
+++ Loops/Print_Letter_Forward.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Print_Letter_Forward{
+    public static void main(String[] args){
+    
+    char ch = 'H';
+    
+    System.out.println("Printing Letters H to N");
+    
+    while(ch <= 'N'){
+        System.out.println(ch);
+        ch++;
+    }
+    
+}
+}
